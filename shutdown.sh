3ddfb63c17e32562e6a4446fb658fc3f55ed52cc
Vitalii <41065091+Angel-if@users.noreply.github.com>
2021-08-04 17:32:01 +0300

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -2,4 +2,4 @@
 systemctl stop tomcat
 cd /opt/tomcat/webapps/
 rm -f ROOT.war client.war  mocks.war startup.sh  shutdown.sh appspec.yml clearup.sh
-rm -rf /opt/codedeploy-agent/deployment-root/deployment-instructions/
+
