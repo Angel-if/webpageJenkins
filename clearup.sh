bd64697e95082d92cbae481eeac5892d1f566a0a
Vitalii <41065091+Angel-if@users.noreply.github.com>
2021-08-04 17:24:11 +0300

    Delete clearup.sh

--- clearup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /opt/tomcat/webapps/
-rm -f ROOT.war client.war  mocks.war startup.sh  shutdown.sh appspec.yml clearup.sh
-rm -rf /opt/codedeploy-agent/deployment-root/deployment-instructions/
