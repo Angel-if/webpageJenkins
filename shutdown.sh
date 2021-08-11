46ac9c25e516472111a872a133c3e4c605a873e9
Vitalii <41065091+Angel-if@users.noreply.github.com>
2021-08-11 14:55:51 +0300

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -2,5 +2,5 @@
 systemctl stop tomcat
 cd /opt/tomcat/webapps/
 #remove files
-rm -f ROOT.war client.war  mocks.war startup.sh appspec.yml  && echo "file has been deleted" > output.txt
+rm -f ROOT.war client.war  mocks.war startup.sh appspec.yml shutdown.sh && echo "file has been deleted" > output.txt
 rm -rf /opt/codedeploy-agent/deployment-root/deployment-instructions/
