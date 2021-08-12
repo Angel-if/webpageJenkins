f09268e3604bc2fb26a054168448fe519d7de327
Vitalii <41065091+Angel-if@users.noreply.github.com>
2021-08-12 11:43:10 +0300

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 systemctl stop tomcat
 cd /opt/tomcat/webapps/
-#remove files
-rm -f ROOT.war client.war  mocks.war startup.sh appspec.yml shutdown.sh && echo "file has been deleted" > output.txt
-rm -rf /opt/codedeploy-agent/deployment-root/deployment-instructions/
+#remove previos version of linked files
+rm -f /opt/tomcat/webapps/ROOT.war /opt/tomcat/webapps/client.war  /opt/tomcat/webapps/mocks.war
+
+#link newer version of files to Tomcat
+ln -s ROOT.war /opt/tomcat/webapps
+Ln -s mocks.war /opt/tomcat/webapps
+ln -s client.war /opt/tomcat/webapps
+
+#Rotate old builds and files
+find /opt/codedeploy-agent/deployment-root/*/* -mtime +1 -exec rm -rf {} \;
