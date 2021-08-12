432c1147f6b267a9099b84709830136b44a852a0
Vitalii <41065091+Angel-if@users.noreply.github.com>
2021-08-12 11:54:22 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+#link newer version of files to Tomcat
+ln -s ROOT.war /opt/tomcat/webapps
+Ln -s mocks.war /opt/tomcat/webapps
+ln -s client.war /opt/tomcat/webapps
+
+sleep 5
 systemctl start tomcat
