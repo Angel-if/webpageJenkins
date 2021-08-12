517d7f8284d5722a98f28f58a5cad58de7979dd9
Vitalii <41065091+Angel-if@users.noreply.github.com>
2021-08-12 15:09:57 +0300

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -5,6 +5,6 @@ cd /opt/tomcat/webapps/
 #remove previos version of linked files
 #rm -f ROOT.war client.war mocks.war startup.sh shutdown.sh appspec.yml
 
-#Rotate old builds and files
+#Rotate old builds and files older 60 min
 find /opt/codedeploy-agent/deployment-root/*/* -mmin +60 -exec rm -rf {} \; 2>/dev/null
 find /opt/codedeploy-agent/deployment-root/* -mmin +60 -exec rm -rf {} \; 2>/dev/null
